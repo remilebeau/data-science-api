697e66acba0365694a589bd75d387bc8438a4ac6
remilebeau <remilebeau90@gmail.com>
2024-06-27 20:41:33 -0400

    add uniform distribution

--- main.py
+++ main.py
@@ -28,14 +28,14 @@ def distribution_triangular(distMin: int, distMode: int, distMax: int):
     if not (distMin <= distMode and distMode <= distMax and distMin < distMax):
         raise HTTPException(
             status_code=400,
-            detail="Please ensure the following: min <= mode <= max and min < max",
+            detail="Please ensure the following: distMin <= distMode <= distMax; and distMin < distMax",
         )
     # generate distribution
     distValues = rng.triangular(distMin, distMode, distMax, 1000).tolist()
     return {"distValues": distValues}
 
 
-# @desc returns the sum of daysPerYear samples from a given triangular distribution to simulate yearly cash flow
+# @desc returns the sum of simPeriodsPerYear samples from a triangular distribution
 # @route GET /api/simulations/
 # @access public
 @app.get("/api/simulations/triangular")
@@ -48,11 +48,80 @@ def simulation_triangular(
     if not (distMin <= distMode and distMode <= distMax and distMin < distMax):
         raise HTTPException(
             status_code=400,
-            detail="Please ensure the following: min <= mode <= max and min < max",
+            detail="Please ensure the following: distMin <= distMode <= distMax; and distMin < distMax",
         )
 
     # generate distribution
     dist = rng.triangular(distMin, distMode, distMax, 1000)
+
+    #   take simPeriodsPerYear samples from the distribution and return their sum. 1000 simValues in total
+    simValues = []
+    for i in range(0, 1000):
+        simValues.append(float(rng.choice(dist, simPeriodsPerYear).sum()))
+
+    # generate stats
+    simMin = round(np.min(simValues))
+    simMax = round(np.max(simValues))
+    simMean = round(np.mean(simValues))
+    simQ1 = round(np.percentile(simValues, 25).round(0))
+    simQ2 = round(np.percentile(simValues, 50).round(0))
+    simQ3 = round(np.percentile(simValues, 75).round(0))
+    lowerCI = round(
+        np.mean(simValues) - 1.96 * np.std(simValues) / np.sqrt(len(simValues))
+    )
+    upperCI = round(
+        np.mean(simValues) + 1.96 * np.std(simValues) / np.sqrt(len(simValues))
+    )
+
+    return {
+        "simValues": simValues,
+        "simMin": simMin,
+        "simMax": simMax,
+        "simMean": simMean,
+        "simQ1": simQ1,
+        "simQ2": simQ2,
+        "simQ3": simQ3,
+        "lowerCI": lowerCI,
+        "upperCI": upperCI,
+    }
+
+
+# @desc returns 1000 random values from a uniform distribution
+# @route GET /api/distributions/uniform
+# @access public
+@app.get("/api/distributions/uniform")
+def distribution_uniform(distMin: int, distMax: int):
+    # set seed
+    rng = np.random.default_rng(seed=42)
+    # check min < max
+    if not (distMin < distMax):
+        raise HTTPException(
+            status_code=400,
+            detail="Please ensure the following: distMin < distMax",
+        )
+    # generate distribution
+    distValues = rng.uniform(distMin, distMax, 1000).tolist()
+    return {"distValues": distValues}
+
+
+# @desc returns the sum of simPeriodsPerYear samples from a uniform distribution
+# @route GET /api/simulations/
+# @access public
+@app.get("/api/simulations/uniform")
+def simulation_uniform(
+    distMin: int, distMode: int, distMax: int, simPeriodsPerYear: int
+):
+    # set seed
+    rng = np.random.default_rng(seed=42)
+    # check min < max
+    if not (distMin < distMax):
+        raise HTTPException(
+            status_code=400,
+            detail="Please ensure the following: distMin < distMax",
+        )
+
+    # generate distribution
+    dist = rng.uniform(distMin, distMode, distMax, 1000)
 
     #   take simPeriodsPerYear samples from the distribution and return their sum. 1000 simValues in total
     simValues = []
