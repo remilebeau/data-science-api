f2410fd70b109f6fe9c0e965af3170410ee72b52
remilebeau <remilebeau90@gmail.com>
2024-07-02 15:58:07 -0400

    replaced simulation routes with a single, more robust simulation route

--- main.py
+++ main.py
@@ -27,69 +27,18 @@ app.add_middleware(
 # @route GET /api/distributions/triangular
 # @access public
 @app.get("/api/distributions/triangular")
-def distribution_triangular(distMin: int, distMode: int, distMax: int):
+def distribution_triangular(distMin: float, distMode: float, distMax: float):
     # set seed
     rng = np.random.default_rng(seed=42)
     # check min <= mode and mode <= max and min < max
     if not (distMin <= distMode and distMode <= distMax and distMin < distMax):
         raise HTTPException(
             status_code=400,
-            detail="Please ensure the following: distMin <= distMode <= distMax; and distMin < distMax",
+            detail="Please ensure the following: 1) distMin <= distMode <= distMax 2) distMin < distMax",
         )
     # generate distribution
     distValues = rng.triangular(distMin, distMode, distMax, 1000).tolist()
     return {"distValues": distValues}
-
-
-# @desc returns the sum of simPeriodsPerYear samples from a triangular distribution, along with stats
-# @route GET /api/simulations/
-# @access public
-@app.get("/api/simulations/triangular")
-def simulation_triangular(
-    distMin: int, distMode: int, distMax: int, simPeriodsPerYear: int
-):
-    # set seed
-    rng = np.random.default_rng(seed=42)
-    # check min <= mode and mode <= max and min < max
-    if not (distMin <= distMode and distMode <= distMax and distMin < distMax):
-        raise HTTPException(
-            status_code=400,
-            detail="Please ensure the following: distMin <= distMode <= distMax; and distMin < distMax",
-        )
-
-    # generate distribution
-    dist = rng.triangular(distMin, distMode, distMax, 1000)
-
-    # create a list of 1000 simulations
-    simValues = [
-        float(rng.choice(dist, simPeriodsPerYear).sum()) for _ in range(0, 1000)
-    ]
-
-    # generate stats
-    simMin = round(np.min(simValues))
-    simMax = round(np.max(simValues))
-    simMean = round(np.mean(simValues))
-    simQ1 = round(np.percentile(simValues, 25).round(0))
-    simQ2 = round(np.percentile(simValues, 50).round(0))
-    simQ3 = round(np.percentile(simValues, 75).round(0))
-    lowerCI = round(
-        np.mean(simValues) - 1.96 * np.std(simValues) / np.sqrt(len(simValues))
-    )
-    upperCI = round(
-        np.mean(simValues) + 1.96 * np.std(simValues) / np.sqrt(len(simValues))
-    )
-
-    return {
-        "simValues": simValues,
-        "simMin": simMin,
-        "simMax": simMax,
-        "simMean": simMean,
-        "simQ1": simQ1,
-        "simQ2": simQ2,
-        "simQ3": simQ3,
-        "lowerCI": lowerCI,
-        "upperCI": upperCI,
-    }
 
 
 # @desc returns 1000 random values from a uniform distribution
@@ -110,61 +59,12 @@ def distribution_uniform(distMin: int, distMax: int):
     return {"distValues": distValues}
 
 
-# @desc returns the sum of simPeriodsPerYear samples from a uniform distribution, along with stats
-# @route GET /api/simulations/
-# @access public
-@app.get("/api/simulations/uniform")
-def simulation_uniform(distMin: int, distMax: int, simPeriodsPerYear: int):
-    # set seed
-    rng = np.random.default_rng(seed=42)
-    # check min < max
-    if not (distMin < distMax):
-        raise HTTPException(
-            status_code=400,
-            detail="Please ensure the following: distMin < distMax",
-        )
-
-    # generate distribution
-    dist = rng.uniform(distMin, distMax, 1000)
-
-    # create a list of 1000 simulations
-    simValues = [
-        float(rng.choice(dist, simPeriodsPerYear).sum()) for _ in range(0, 1000)
-    ]
-
-    # generate stats
-    simMin = round(np.min(simValues))
-    simMax = round(np.max(simValues))
-    simMean = round(np.mean(simValues))
-    simQ1 = round(np.percentile(simValues, 25).round(0))
-    simQ2 = round(np.percentile(simValues, 50).round(0))
-    simQ3 = round(np.percentile(simValues, 75).round(0))
-    lowerCI = round(
-        np.mean(simValues) - 1.96 * np.std(simValues) / np.sqrt(len(simValues))
-    )
-    upperCI = round(
-        np.mean(simValues) + 1.96 * np.std(simValues) / np.sqrt(len(simValues))
-    )
-
-    return {
-        "simValues": simValues,
-        "simMin": simMin,
-        "simMax": simMax,
-        "simMean": simMean,
-        "simQ1": simQ1,
-        "simQ2": simQ2,
-        "simQ3": simQ3,
-        "lowerCI": lowerCI,
-        "upperCI": upperCI,
-    }
-
-
 # @desc returns 1000 random values from a truncated normal distribution
 # @route GET /api/distributions/truncated_normal
 # @access public
 @app.get("/api/distributions/truncated_normal")
 def distribution_truncated_normal(
-    distMin: int, distMean: int, distMax: int, distSD: float
+    distMin: float, distMean: float, distMax: float, distSD: float
 ):
     # validate data
     if distSD < 0:
@@ -176,81 +76,121 @@ def distribution_truncated_normal(
     # set seed
     rng = np.random.default_rng(seed=42)
 
-    # generate normal distribution to truncate
-    norm_vals = rng.normal(distMean, distSD, 1000).tolist()
-
     # truncate with custom function
-    def truncated_normal(norm_vals: list):
-        value = rng.choice(norm_vals)
-        if not (distMin <= value and value <= distMax):
-            value = truncated_normal(norm_vals)
+    def truncated_normal(min, max, mean, sd):
+        value = rng.normal(mean, sd)
+        if value < min or value > max:
+            value = truncated_normal(min, max, mean, sd)
         return value
 
     # generate distribution
-    distValues = [truncated_normal(norm_vals) for _ in range(0, 1000)]
+    distValues = [
+        truncated_normal(distMin, distMax, distMean, distSD) for _ in range(0, 1000)
+    ]
 
     return {"distValues": distValues}
 
 
-# @desc returns the sum of simPeriodsPerYear samples from a truncated normal distribution, along with stats
-# @route GET /api/simulations/
+# @desc monte carlo simulation of planning production by Tallys Yunes. using triangular distribution instead of truncated normal. confidence intervals are 95%
+# @route GET /api/simulations/monte_carlo
 # @access public
-@app.get("/api/simulations/truncated_normal")
-def simulation_truncated_normal(
-    distMin: int, distMean: int, distMax: int, distSD: float, simPeriodsPerYear: int
+@app.get("/api/simulations/monte_carlo")
+def simulation_monte_carlo(
+    unitCost: float,
+    unitPrice: float,
+    salvagePrice: float,
+    demandMin: float,
+    demandMode: float,
+    demandMax: float,
+    fixedCost: float,
+    productionQuantity: float,
 ):
     # validate data
-    if distSD < 0:
+    if not (
+        demandMin <= demandMode
+        and demandMode <= demandMax
+        and demandMin < demandMax
+        and productionQuantity > 0
+    ):
         raise HTTPException(
             status_code=400,
-            detail="Standard deviation must be non-negative",
+            detail="Please ensure the following: 1) demandMin <= demandMode <= demandMax 2) demandMin < demandMax 3) productionQuantity > 0",
         )
 
     # set seed
     rng = np.random.default_rng(seed=42)
 
-    # generate normal distribution to truncate
-    norm_vals = rng.normal(distMean, distSD, 1000).tolist()
+    # simulation function
+    def calculate_profit(
+        unitCost: float,
+        unitPrice: float,
+        salvagePrice: float,
+        demandMin: float,
+        demandMode: float,
+        demandMax: float,
+        fixedCost: float,
+        productionQuantity: float,
+    ):
+        # profit = revenues - costs = sales rev + salvage rev - production cost - fixed costs
+        demand_distribution = rng.triangular(demandMin, demandMode, demandMax, 1000)
+        realized_demand = float(rng.choice(demand_distribution, 1).sum())
+        units_sold = min(realized_demand, productionQuantity)
+        units_salvaged = max(productionQuantity - realized_demand, 0)
+        production_cost = productionQuantity * unitCost
+        revenue_from_sales = units_sold * unitPrice
+        revenue_from_salvage = units_salvaged * salvagePrice
+        profit = revenue_from_sales + revenue_from_salvage - production_cost - fixedCost
+        return profit
 
-    # truncate with custom function
-    def truncated_normal(norm_vals: list):
-        value = rng.choice(norm_vals)
-        if not (distMin <= value and value <= distMax):
-            value = truncated_normal(norm_vals)
-        return value
-
-    # generate distribution
-    trunc_vals = []
-    for i in range(0, 1000):
-        trunc_vals.append(truncated_normal(norm_vals))
-
-    # create a list of 1000 simulations
-    simValues = [
-        float(rng.choice(trunc_vals, simPeriodsPerYear).sum()) for _ in range(0, 1000)
+    # run 1000 simulations
+    simulated_profits = [
+        calculate_profit(
+            unitCost,
+            unitPrice,
+            salvagePrice,
+            demandMin,
+            demandMode,
+            demandMax,
+            fixedCost,
+            productionQuantity,
+        )
+        for _ in range(0, 1000)
     ]
 
     # generate stats
-    simMin = round(np.min(simValues))
-    simMax = round(np.max(simValues))
-    simMean = round(np.mean(simValues))
-    simQ1 = round(np.percentile(simValues, 25).round(0))
-    simQ2 = round(np.percentile(simValues, 50).round(0))
-    simQ3 = round(np.percentile(simValues, 75).round(0))
-    lowerCI = round(
-        np.mean(simValues) - 1.96 * np.std(simValues) / np.sqrt(len(simValues))
+    mean = round(np.mean(simulated_profits))
+    stdError = round(1.96 * np.std(simulated_profits) / np.sqrt(len(simulated_profits)))
+    lowerCI = round(mean - stdError)
+    upperCI = round(mean + stdError)
+    min_profit = round(np.min(simulated_profits))
+    max_profit = round(np.max(simulated_profits))
+    q1 = round(np.percentile(simulated_profits, 25))
+    q2 = round(np.percentile(simulated_profits, 50))
+    q3 = round(np.percentile(simulated_profits, 75))
+    p_lose_money = [profit < 0 for profit in simulated_profits].count(True) / len(
+        simulated_profits
     )
-    upperCI = round(
-        np.mean(simValues) + 1.96 * np.std(simValues) / np.sqrt(len(simValues))
+    p_lose_money_lower_ci = round(
+        p_lose_money
+        - 1.96 * np.sqrt(p_lose_money * (1 - p_lose_money) / len(simulated_profits)),
+        2,
     )
-
+    p_lose_money_upper_ci = round(
+        p_lose_money
+        + 1.96 * np.sqrt(p_lose_money * (1 - p_lose_money) / len(simulated_profits)),
+        2,
+    )
     return {
-        "simValues": simValues,
-        "simMin": simMin,
-        "simMax": simMax,
-        "simMean": simMean,
-        "simQ1": simQ1,
-        "simQ2": simQ2,
-        "simQ3": simQ3,
+        "simulatedProfits": simulated_profits,
+        "meanProfit": mean,
+        "stdError": stdError,
         "lowerCI": lowerCI,
         "upperCI": upperCI,
+        "minProfit": min_profit,
+        "maxProfit": max_profit,
+        "q1": q1,
+        "q2": q2,
+        "q3": q3,
+        "pLoseMoneyLowerCI": p_lose_money_lower_ci,
+        "pLoseMoneyUpperCI": p_lose_money_upper_ci,
     }
