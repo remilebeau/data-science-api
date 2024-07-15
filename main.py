c683ef9e4b2ae1bc42cbc8c1aadeeef5e74c975b
remilebeau <remilebeau90@gmail.com>
2024-07-15 19:07:26 -0400

    update docstring for finance and production simulations

--- main.py
+++ main.py
@@ -181,13 +181,13 @@ def simulation_production(
 
     Returns:\n
         simulatedProfits (list): A list of 1000 simulated profits.\n
-        meanProfit (float): The expected of the 1000 simulated profits.\n
+        meanProfit (float): The expected profit.\n
         meanStandardError (float): The standard error of the expected profit.\n
         meanLowerCI (float): The lower 95% confidence interval for the expected profit.\n
         meanUpperCI (float): The upper 95% confidence interval for the expected profit.\n
         pLoseMoneyLowerCI (float): The lower 95% confidence interval for the probability of a negative profit.\n
         pLoseMoneyUpperCI (float): The upper 95% confidence interval for the probability of a negative profit.\n
-        valueAtRisk (float): The value at risk at the 5% level.\n
+        valueAtRisk (float): The value at risk at the 5% level. Returns 0 if value at risk is positive.\n
 
     Raises:\n
         HTTPException: If the inputs do not satisfy the following conditions:
@@ -269,7 +269,7 @@ def simulation_finance(
     discountRate: float,
 ):
     """
-    Simulates a financial project and returns a 95% confidence interval of the mean NPV, a 95% confidence interval of the probability of a negative NPV, and the value at risk at the 5% level based on the simulation results. Planning horizon = 5 years. Sales and sales decay follow a triangular distribution. 1000 simulations.
+    Simulates a financial project and returns a 95% confidence interval of the expected NPV, a 95% confidence interval of the probability of a negative NPV, and the value at risk at the 5% level based on the simulation results. Planning horizon = 5 years. Sales and sales decay follow a triangular distribution. 1000 simulations.
 
     Args:\n
         fixedCost (float): The total fixed cost of the project.\n
@@ -277,12 +277,12 @@ def simulation_finance(
         yearOneSalesMin (float): Minimum sales in year 1.\n
         yearOneSalesMode (float): Expected sales in year 1.\n
         yearOneSalesMax (float): Maximum sales in year 1.\n
-        annualMarginDecrease (float): The annual margin decrease in years 2 to 5. Does not change from year to year.\n
-        annualSalesDecayMin (float): The minimum annual sales decay in years 2 to 5. Set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 to disregard annual sales decay.\n
-        annualSalesDecayMode (float): The expected annual sales decay in years 2 to 5.\n
-        annualSalesDecayMax (float): The maximum annual sales decay in years 2 to 5.\n
-        taxRate (float): The tax rate.\n
-        discountRate (float): The discount rate.\n
+        annualMarginDecrease (float): The annual margin decrease in years 2 to 5. Does not change from year to year. Set to 0 to remove from model.\n
+        annualSalesDecayMin (float): The minimum annual sales decay in years 2 to 5. Set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 remove from model.\n
+        annualSalesDecayMode (float): The expected annual sales decay in years 2 to 5. Set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 remove from model.\n
+        annualSalesDecayMax (float): The maximum annual sales decay in years 2 to 5. Set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 remove from model.\n
+        taxRate (float): The tax rate. Set to 0 to remove from model.\n
+        discountRate (float): The discount rate. Set to 0 to remove from model.\n
 
     Returns:\n
         simulatedNPVs (list): A list of simulated NPVs
@@ -292,7 +292,7 @@ def simulation_finance(
         meanUpperCI (float): The upper 95% confidence interval of the mean NPV
         pLoseMoneyLowerCI (float): The lower 95% confidence interval of the probability of losing money
         pLoseMoneyUpperCI (float): The upper 95% confidence interval of the probability of losing money
-        valueAtRisk (float): The value at risk at the 5% level
+        valueAtRisk (float): The value at risk at the 5% level. Returns 0 if value at risk is positive.
 
     Raises:\n
         HTTPException: If the input values do not satisfy the following conditions:
@@ -323,7 +323,7 @@ def simulation_finance(
     ):
         raise HTTPException(
             status_code=400,
-            detail="Please check that annualSalesDecayMin <= annualSalesDecayMode <= annualSalesDecayMax and annualSalesDecayMin < annualSalesDecayMax. Or set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 to disregard annual sales decay.",
+            detail="Please check that annualSalesDecayMin <= annualSalesDecayMode <= annualSalesDecayMax and annualSalesDecayMin < annualSalesDecayMax. Or set annualSalesDecayMin, annualSalesDecayMode, and annualSalesDecayMax all to 0 to remove from model.",
         )
     # taxRate must be a percentage
     if not is_percent(taxRate):
