8e4886bd6e3fe436795be916857c762e7a4ea7f0
remilebeau <remilebeau90@gmail.com>
2024-07-20 12:26:46 -0400

    add tests to util functions

---
+++ utils/test_utils.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+from .utils import is_triangular, is_percent, is_all_zero
+from ..main import app
+
+client = TestClient(app)
+
+
+def test_is_triangular():
+    assert is_triangular(1, 2, 3)
+    assert not is_triangular(3, 2, 1)
+    assert not is_triangular(2, 1, 3)
+
+
+def test_is_percent():
+    assert is_percent(0.5)
+    assert not is_percent(1.5)
+    assert not is_percent(-0.5)
+
+
+def test_is_all_zero():
+    assert is_all_zero(0, 0, 0)
+    assert not is_all_zero(1, 0, 0)
+    assert not is_all_zero(0, 0, 1)
+    assert not is_all_zero(0, 1, 0)
+    assert not is_all_zero(1, 1, 1)
