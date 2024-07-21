efe7b4baff38d7f05353a0c799332f7c2299bc0e
remilebeau <remilebeau90@gmail.com>
2024-07-21 00:13:48 -0400

    rewrite is_triangular() and is_percent()

--- utils/utils.py
+++ utils/utils.py
@@ -3,11 +3,11 @@ import numpy as np
 
 # utility functions for validation
 def is_triangular(min: float, mode: float, max: float):
-    return min <= mode and mode <= max and min < max
+    return (min <= mode <= max) and min < max
 
 
 def is_percent(num: float):
-    return num >= 0 and num <= 1
+    return 0 <= num <= 1
 
 
 def is_all_zero(*args):
