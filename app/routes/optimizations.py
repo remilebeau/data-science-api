790c745f934320ca35e1146d8c0e3c273a68a805
remilebeau <remilebeau90@gmail.com>
2025-05-28 18:51:21 -0400

    simplify optimization model

--- app/routes/optimizations.py
+++ app/routes/optimizations.py
@@ -11,13 +11,6 @@ class Constraints(BaseModel):
     friReq: int
     satReq: int
     sunReq: int
-    x1Max: int
-    x2Max: int
-    x3Max: int
-    x4Max: int
-    x5Max: int
-    x6Max: int
-    x7Max: int
 
 
 router = APIRouter(
@@ -31,27 +24,36 @@ router = APIRouter(
 # @access public
 @router.post("/staffing")
 def optimization_staffing(constraints: Constraints):
+    """
+    X1 = number of Mon to Fri workers\n
+    X2 = number of Tue to Sat workers\n
+    X3 = number of Wed to Sun workers\n
+    X4 = number of Thu to Mon workers\n
+    X5 = number of Fri to Tue workers\n
+    X6 = number of Sat to Wed workers\n
+    X7 = number of Sun to Thu workers\n
+    Minimize X1 + X2 + X3 + X4 + X5 + X6 + X7\n
+    s.t.\n
+    Mon staff available >= Mon staff required\n
+    Tue staff available >= Tue staff required\n
+    Wed staff available >= Wed staff required\n
+    Thu staff available >= Thu staff required\n
+    Fri staff available >= Fri staff required\n
+    Sat staff available >= Sat staff required\n
+    Sun staff available >= Sun staff required\n
+    """
 
     # create solver
     solver = pywraplp.Solver.CreateSolver("SCIP")
 
-    # define upper bounds, if provided
-    x1Ub = constraints.x1Max if constraints.x1Max >= 0 else solver.Infinity()
-    x2Ub = constraints.x2Max if constraints.x2Max >= 0 else solver.Infinity()
-    x3Ub = constraints.x3Max if constraints.x3Max >= 0 else solver.Infinity()
-    x4Ub = constraints.x4Max if constraints.x4Max >= 0 else solver.Infinity()
-    x5Ub = constraints.x5Max if constraints.x5Max >= 0 else solver.Infinity()
-    x6Ub = constraints.x6Max if constraints.x6Max >= 0 else solver.Infinity()
-    x7Ub = constraints.x7Max if constraints.x7Max >= 0 else solver.Infinity()
-
     # decision variables
-    x1 = solver.IntVar(lb=0, ub=x1Ub, name="x1")
-    x2 = solver.IntVar(lb=0, ub=x2Ub, name="x2")
-    x3 = solver.IntVar(lb=0, ub=x3Ub, name="x3")
-    x4 = solver.IntVar(lb=0, ub=x4Ub, name="x4")
-    x5 = solver.IntVar(lb=0, ub=x5Ub, name="x5")
-    x6 = solver.IntVar(lb=0, ub=x6Ub, name="x6")
-    x7 = solver.IntVar(lb=0, ub=x7Ub, name="x7")
+    x1 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x1")
+    x2 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x2")
+    x3 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x3")
+    x4 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x4")
+    x5 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x5")
+    x6 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x6")
+    x7 = solver.IntVar(lb=0, ub=solver.Infinity(), name="x7")
     # objective function
     min_staff = x1 + x2 + x3 + x4 + x5 + x6 + x7
     # constraints
@@ -90,9 +92,7 @@ def optimization_staffing(constraints: Constraints):
     friSlack = friAva.solution_value() - friReq
     satSlack = satAva.solution_value() - satReq
     sunSlack = sunAva.solution_value() - sunReq
-    totalSlack = (
-        monSlack + tueSlack + wedSlack + thuSlack + friSlack + satSlack + sunSlack
-    )
+    totalSlack = monSlack + tueSlack + wedSlack + thuSlack + friSlack + satSlack + sunSlack
     # print results
     if status == pywraplp.Solver.OPTIMAL:
         return {
--- app/routes/test_optimizations.py
+++ app/routes/test_optimizations.py
@@ -5,7 +5,7 @@ from ..main import app
 client = TestClient(app)
 
 
-# @DESC Test staffing optimization with no upper bounds on decision variables
+# @DESC Test staffing optimization
 # @ROUTE POST /api/optimizations/staffing
 def test_staffing_optimization_no_max():
     body = {
@@ -16,42 +16,9 @@ def test_staffing_optimization_no_max():
         "friReq": 14,
         "satReq": 16,
         "sunReq": 11,
-        "x1Max": -1,
-        "x2Max": -1,
-        "x3Max": -1,
-        "x4Max": -1,
-        "x5Max": -1,
-        "x6Max": -1,
-        "x7Max": -1,
     }
     res = client.post("/api/optimizations/staffing", json=body)
     minStaff = res.json()["minStaff"]
     assert res.status_code == 200
     # check for accuracy. answer should be between 22 and 24
     assert 22 <= minStaff <= 24
-
-
-# @DESC Test staffing optimization with upper bounds on decision variables
-# @ROUTE POST /api/optimizations/staffing
-def test_staffing_optimization():
-    body = {
-        "monReq": 17,
-        "tueReq": 13,
-        "wedReq": 15,
-        "thuReq": 19,
-        "friReq": 14,
-        "satReq": 16,
-        "sunReq": 11,
-        "x1Max": -1,
-        "x2Max": -1,
-        "x3Max": -1,
-        "x4Max": 0,
-        "x5Max": -1,
-        "x6Max": -1,
-        "x7Max": -1,
-    }
-    res = client.post("/api/optimizations/staffing", json=body)
-    minStaff = res.json()["minStaff"]
-    assert res.status_code == 200
-    # check for accuracy. answer should be between 25 and 27
-    assert 25 <= minStaff <= 27
