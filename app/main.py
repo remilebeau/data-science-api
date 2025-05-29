dc77822cafcf48467e710f9688e7831056b17e15
remilebeau <remilebeau90@gmail.com>
2025-05-29 00:28:37 -0400

    add root landing page for api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 from .routes import optimizations, simulations
 
@@ -20,3 +21,20 @@ app.add_middleware(
 # routes
 app.include_router(simulations.router)
 app.include_router(optimizations.router)
+
+# root page
+
+
+@app.get("/", include_in_schema=False)
+def custom_homepage():
+    html_content = """
+    <html>
+        <head><title>Production Simulation API</title></head>
+        <body>
+            <h1>Welcome to the Production Simulation and Staffing Optimization API</h1>
+            <p>Developed by remilebeau</p>
+            <p>Visit <a href='/docs'>Swagger UI</a> for interactive documentation.</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
